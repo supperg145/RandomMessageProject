9c14f3a675642d10dc1580fddaa44333e5159f0e
supperg <gabor1415@gmail.com>
2023-07-19 12:21:56 +0200

    added messages

---
+++ script.js
@@ -0,0 +1,4 @@
+let having = ["bad luck", "good luck", "the worst day", "the best day", "a tough", "an easy"]
+let shouldDo = ["go bowling", "get a scratch off ticket", "take the day off", "go camping", "appreciate what you have"]
+let receive = ["money", "happiness", "a raise", "the love of your life", "a ticekt to Disneyland", "plane tickets to Amazonia"]
+
